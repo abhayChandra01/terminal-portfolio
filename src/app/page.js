a66493b06259f99e2d0943039ccca1b77c1229d0
abhayChandra01 <abhayxelp@gmail.com>
2023-07-28 11:37:13 +0530

    improvements

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [terminalShow, setTerminalShow] = useState(true)
 
   return (
-    <div className='flex relative justify-center items-center w-100% min-h-screen bg-[url(../assets/images/bgimage.jpg)]'>
+    <div className='font-mono tracking-tighter flex relative justify-center items-center w-100% min-h-screen bg-[url(../assets/images/bgimage.jpg)]'>
       <Header />
       {terminalShow ?
         <Terminal />
